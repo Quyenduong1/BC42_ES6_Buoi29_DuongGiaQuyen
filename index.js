f79ef34516e9c07113aa44212d486efb4913eb9d
Quyen <dgiaquyen40@gmail.com>
2023-03-19 10:58:55 +0700

    dang lam

---
+++ index.js
@@ -0,0 +1,114 @@
+function myFuntion() {
+    let chose = document.getElementById("chucvu").value;
+    if(chose === "Student"){
+        const subject = `
+        <div class="form-group">
+			<div class="input-group">
+                <div class="input-group-prepend">
+					<span class="input-group-text"><i class="fa-solid fa-clock"></i></span>
+				</div>
+				<input type="number" name="toan" id="subject" class="form-control input-sm "
+				placeholder="Điểm toán">
+                                    
+			</div>
+				<span class="sp-thongbao" id="tbDiemToan"></span>
+		</div>
+
+        <div class="form-group" >
+			<div class="input-group">
+				<div class="input-group-prepend">
+					<span class="input-group-text"><i class="fa-solid fa-clock"></i></span>
+				</div>
+				<input type="number" name="ly"  class="form-control input-sm "
+				placeholder="Điểm Lý">
+			</div>
+				<span class="sp-thongbao" id="tbDiemLy"></span>
+		</div>
+
+        <div class="form-group">
+			<div class="input-group">
+				<div class="input-group-prepend">
+					<span class="input-group-text"><i class="fa-solid fa-clock"></i></span>
+				</div>
+				<input type="number" name="hoa"   class="form-control input-sm "
+				placeholder="Điểm Hóa">
+			</div>
+			<span class="sp-thongbao" id="tbDiemHoa"></span>
+		</div>
+
+        `;
+        document.getElementById('employee').innerHTML = "";
+        document.getElementById('customer').innerHTML = "";
+        document.getElementById('student-subject').innerHTML = subject;
+    } else if(chose === 'Employee'){
+        const employee = `
+        <div class="form-group" >
+			<div class="input-group">
+				<div class="input-group-prepend">
+					<span class="input-group-text"><i class="fa-solid fa-clock"></i></span>
+				</div>
+				<input type="number" name="timework"  class="form-control input-sm "
+				placeholder="Số ngày làm việc">
+			</div>
+				<span class="sp-thongbao" id="tbTimeWork"></span>
+		</div>
+
+        <div class="form-group" >
+			<div class="input-group">
+				<div class="input-group-prepend">
+					<span class="input-group-text"><i class="fa-solid fa-clock"></i></span>
+				</div>
+				<input type="number" name="luong"  class="form-control input-sm "
+				placeholder="Lương theo ngày">
+			</div>
+				<span class="sp-thongbao" id="tbLuong"></span>
+		</div>
+        `;
+        document.getElementById('student-subject').innerHTML = "";
+        document.getElementById('customer').innerHTML = "";
+        document.getElementById('employee').innerHTML = employee;
+    } else if(chose === 'Customer'){
+        const customer = `
+        <div class="form-group" >
+			<div class="input-group">
+				<div class="input-group-prepend">
+					<span class="input-group-text"><i class="fa-solid fa-clock"></i></span>
+				</div>
+				<input type="text" name="company"  class="form-control input-sm "
+				placeholder="Tên công ty">
+			</div>
+				<span class="sp-thongbao" id="tbCompany"></span>
+		</div>
+
+        <div class="form-group" >
+			<div class="input-group">
+				<div class="input-group-prepend">
+					<span class="input-group-text"><i class="fa-solid fa-clock"></i></span>
+				</div>
+				<input type="number" name="price"  class="form-control input-sm "
+				placeholder="Trị giá hóa đơn">
+			</div>
+				<span class="sp-thongbao" id="tbPrice"></span>
+		</div>
+
+        <div class="form-group" >
+			<div class="input-group">
+				<div class="input-group-prepend">
+					<span class="input-group-text"><i class="fa-solid fa-clock"></i></span>
+				</div>
+				<input type="number" name="danhgia"  class="form-control input-sm "
+				placeholder="Đánh giá">
+			</div>
+				<span class="sp-thongbao" id="tbDanhGia"></span>
+		</div>
+
+        `;
+        document.getElementById('student-subject').innerHTML = "";
+        document.getElementById('employee').innerHTML = "";
+        document.getElementById('customer').innerHTML = customer;
+    } else {
+        document.getElementById('student-subject').innerHTML = "";
+        document.getElementById('employee').innerHTML = "";
+        document.getElementById('customer').innerHTML = "";
+    }
+}
